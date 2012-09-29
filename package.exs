6e9506a3578441fc958c447931ad98bb3afc788e
Yurii Rashkovskii <yrashk@spawngrid.com>
2012-09-28 23:10:50 -0700

    Add expm package.exs

---
+++ package.exs
@@ -0,0 +1,4 @@
+Expm.Package.new(name: "xonad", description: "(Functionless) Monad-style macros for Elixir",
+                 version: "0.1", keywords: ["Monad","experiment","macros"],
+                 maintainers: [[name: "Yurii Rashkovskii", email: "yrashk@gmail.com"]],
+                 repositories: [[github: "yrashk/xonad", tag: "0.1"]])
